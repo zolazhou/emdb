4efc4a88d302e14822d5d6c189eb974b5a421e2a
Zola Zhou <zolazhou@gmail.com>
2013-05-16 17:49:59 +0800

    add more tests

--- test/emdb_tests.erl
+++ test/emdb_tests.erl
@@ -57,15 +57,47 @@ cursor_test() ->
 
         {ok, Txn2} = emdb:txn_begin(Env),
         {ok, Cursor2} = emdb:cursor_open(Txn2, DB),
-        {ok, {<<"key1">>, <<"data1">>}} = emdb:cursor_get(Cursor2, next),
+        {ok, {<<"key2">>, <<"data2">>}} = emdb:cursor_get(Cursor2, <<"key2">>, undefined, set),
         ok = emdb:cursor_del(Cursor2),
         ok = emdb:cursor_close(Cursor2),
         ok = emdb:txn_commit(Txn2),
 
         {ok, Txn3} = emdb:txn_begin(Env),
-        {error, not_found} = emdb:get(Txn3, DB, <<"key1">>),
-        {ok, <<"data2">>} = emdb:get(Txn3, DB, <<"key2">>),
+        {ok, <<"data1">>} = emdb:get(Txn3, DB, <<"key1">>),
+        {error, not_found} = emdb:get(Txn3, DB, <<"key2">>),
+        {ok, <<"data3">>} = emdb:get(Txn3, DB, <<"key3">>),
         ok = emdb:txn_commit(Txn3),
+
+        ok = emdb:close(DB)
+    after
+        ok = emdb:env_close(Env),
+        os:cmd("rm -rf ./testdb")
+    end.
+
+iter_test() ->
+    os:cmd("mkdir ./testdb"),
+    {ok, Env} = emdb:env_create(),
+    try
+        ok = emdb:env_set_maxdbs(Env, 4),
+        ok = emdb:env_open(Env, "./testdb"),
+        {ok, Txn} = emdb:txn_begin(Env),
+        {ok, DB}  = emdb:open(Txn, "db1", [create]),
+
+        {ok, Cursor} = emdb:cursor_open(Txn, DB),
+        ok = emdb:cursor_put(Cursor, <<"key1">>, <<"data1">>, [no_overwrite]),
+        ok = emdb:cursor_put(Cursor, <<"key2">>, <<"data2">>, [no_overwrite]),
+        ok = emdb:cursor_put(Cursor, <<"key4">>, <<"data4">>, [no_overwrite]),
+        ok = emdb:cursor_put(Cursor, <<"key5">>, <<"data5">>, [no_overwrite]),
+        ok = emdb:cursor_close(Cursor),
+        ok = emdb:txn_commit(Txn),
+
+        {ok, Txn1} = emdb:txn_begin(Env),
+        {ok, Cursor1} = emdb:cursor_open(Txn1, DB),
+        {error, not_found} = emdb:cursor_get(Cursor1, <<"key3">>, undefined, set),
+        {error, not_found} = emdb:cursor_get(Cursor1, <<"key3">>, undefined, set_key),
+        {ok, {<<"key4">>, <<"data4">>}} = emdb:cursor_get(Cursor1, <<"key3">>, undefined, set_range),
+        ok = emdb:cursor_close(Cursor1),
+        ok = emdb:txn_commit(Txn1),
 
         ok = emdb:close(DB)
     after
